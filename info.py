30180c1a0bb0da6c8726bf600cfd8e95ee3ce627
Sathyanath Nandakumar <43042869+Sathyanath42@users.noreply.github.com>
2019-11-20 16:58:11 -0500

    Delete info.py

--- info.py
+++
@@ -1,8 +0,0 @@
-def info_gain(left, right, current_uncertainty):
-    """Information Gain.
-
-    The uncertainty of the starting node, minus the weighted impurity of
-    two child nodes.
-    """
-    p = float(len(left)) / (len(left) + len(right))
-    return current_uncertainty - p * gini(left) - (1 - p) * gini(right)
