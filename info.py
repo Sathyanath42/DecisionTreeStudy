df90560080129ebc95a5169e26e13fdb235d718d
Sathyanath Nandakumar <43042869+Sathyanath42@users.noreply.github.com>
2019-11-20 16:53:41 -0500

    Create info.py

---
+++ info.py
@@ -0,0 +1,8 @@
+def info_gain(left, right, current_uncertainty):
+    """Information Gain.
+
+    The uncertainty of the starting node, minus the weighted impurity of
+    two child nodes.
+    """
+    p = float(len(left)) / (len(left) + len(right))
+    return current_uncertainty - p * gini(left) - (1 - p) * gini(right)
