74b1958699cc060f4c6648234cbb18a833008461
Sathyanath Nandakumar <43042869+Sathyanath42@users.noreply.github.com>
2019-12-03 12:28:59 -0500

    Update gini.py

--- gini.py
+++ gini.py
@@ -1,8 +1,32 @@
-def gini(rows):
-   
-    counts = class_counts(rows)
+def cls_cnt(items):   # creates a dictionary where the keys are the unique terms in the
+    cnt = dict()      # column and the values they relate to are the counts of that term
+    for c in items:
+        out = c[-1]
+        cnt[out] = cnt.get(out, 0) + 1
+    return cnt
+
+# get(key,value to return if key doesnt exist) method returns the value of the item with the specified key
+# {0: 1}
+# {0: 2}
+# {0: 3}
+# ...
+# {0: 190, 1: 1}
+# {0: 190, 1: 2}
+# {0: 190, 1: 3}
+# ...
+# final result :  {0: 190, 1: 136}
+
+
+def gini(data):
     impurity = 1
-    for lbl in counts:
-        prob_of_lbl = counts[lbl] / float(len(rows))
-        impurity -= prob_of_lbl**2
+    counts = cls_cnt(data)
+    for t in counts:
+        prob_val = counts[t] / float(len(data))
+        impurity = impurity - prob_val**2
     return impurity
+
+# probability of a value is given by its count in the column (accessed within the dictionary)
+# divided by the len of the data set
+# This value is subtracted from the impurity which was originally 1
+# b = gini(tdata)
+# >>> 0.48628100417780123 (the gini value for the original data set)
